89b76422d6bb9e1d422f6023ea62c012b3972bb6
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-17 19:42:06 +0000

    Wrote a new szenario getting to creating a player.

--- features/starting_a_game.feature
+++ features/starting_a_game.feature
@@ -8,5 +8,10 @@ Feature: Starting the game
 		When I follow the "New Game"
 		Then I should see "Welcome to Battleships!"
 
+  Scenario: Creating a player
+    Given I am on new_game page
+    When I click on "Create player"
+    Then I should see: "What's your name?"
+
 
 
--- features/step_definitions/battleships_steps.rb
+++ features/step_definitions/battleships_steps.rb
@@ -10,3 +10,18 @@ Then(/^I should see "(.*?)"$/) do |arg1|
   expect(page).to have_content(arg1)
 end
 
+
+
+
+
+Given(/^I am on new_game page$/) do
+  visit('new_game')
+end
+
+When(/^I click on "(.*?)"$/) do |create_player|
+  click_link(create_player)
+end
+
+Then(/^I should see: "(.*?)"$/) do |arg1|
+  expect(page).fill_in('Player name', :with => "John")
+end
