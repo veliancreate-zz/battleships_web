c182117881d6d87dc7ac9425eac2db60db29285b
veliancreate <julian_veling@hotmail.com>
2015-02-19 16:48:59 +0000

    using constant variables and added computer player

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -8,6 +8,10 @@ require_relative 'water'
 
 class BattleShips < Sinatra::Base
   
+  GAME = Game.new
+  BOARD = Board.new(Cell)
+  COMPUTER_BOARD = Board.new(Cell)
+
   enable :sessions
 
   get '/' do
@@ -15,29 +19,42 @@ class BattleShips < Sinatra::Base
   end
 
   get '/new_game' do
+    
     erb :game
   end  
 
   post '/new_game' do
     if params[:name]
-      player = session[:player]=Player.new
-      @player = player
+      @player = Player.new
+      @computer = Player.new
+      @computer.board= COMPUTER_BOARD
       @player.name=params[:name]
-      @board=Board.new(Cell)
+      @player.board = BOARD
+      GAME.add_player(@player)
+      GAME.add_player(@computer)
     end 
-
+    @board = BOARD
     def push_water  
       ('A'..'J').each do |row|  
         (1..10).each do |col|
           joined = row+col.to_s
           joined_sym = joined.to_sym
-          if !@board.grid[joined_sym].content.is_a?(Ship) then @board.grid[joined_sym].content = Water.new end
+          if !@computer.board.grid[joined_sym].content.is_a?(Ship) then @computer.board.grid[joined_sym].content = Water.new end
         end
       end 
     end  
+    push_water
 
     erb :game
   end 
+
+  post '/game' do
+    @game=GAME
+    @player = GAME.player1
+    
+    @shot = params[:shoot_on].to_sym
+
+  end  
 
   set :views, Proc.new{File.join(root, "..", "views")}
 
